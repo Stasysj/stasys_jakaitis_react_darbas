263a90f3d8eccc35d6deb7246b5b3fbfb9b90054
Stasys <srasys@st.lt>
2022-07-04 13:45:50 +0300

    LoginPage_more_css

--- src/components/LoginForm/LoginForm.jsx
+++ src/components/LoginForm/LoginForm.jsx
@@ -4,9 +4,19 @@ import css from './LoginForm.module.css';
 function LoginForm() {
   return (
     <form className={css.form}>
-      <h1>Login</h1>
-      <input className={css.input} type='email' />
-      <input className={css.input} type='password' />
+      <h1 className={css.title}>Welcome back,</h1>
+
+      <label className={css.label}>
+        <span className={css.span}>Email</span>
+        <input className={css.input} type='email' />
+      </label>
+      <label className={css.label}>
+        <span className={css.span}>Password</span>
+        <input className={css.input} type='password' />
+      </label>
+      {/* <input className={css.input} type='email' />
+      <input className={css.input} type='password' /> */}
+      <p class={css.forgot_pass}>Forgot password?</p>
       <button className={css.btn} type='submit'>
         Login
       </button>
