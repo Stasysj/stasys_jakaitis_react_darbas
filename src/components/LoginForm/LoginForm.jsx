6395d2077b30c5bb045f73bc7f0f9404f620d647
Stasys <srasys@st.lt>
2022-07-04 14:46:15 +0300

    add_formikand_yup_log_val_ok

--- src/components/LoginForm/LoginForm.jsx
+++ src/components/LoginForm/LoginForm.jsx
@@ -1,22 +1,58 @@
-import React from 'react';
 import css from './LoginForm.module.css';
+import { useFormik } from 'formik';
+import React from 'react';
 
+import * as Yup from 'yup';
+
+// -----------------------------
+const initValues = {
+  email: 'test@email.com',
+  password: '123456',
+};
+// -------------------------------
 function LoginForm() {
+  const formik = useFormik({
+    initialValues: initValues,
+    validationSchema: Yup.object({
+      email: Yup.string().email('Patikrinkite savo email').required(),
+      password: Yup.string().min(4, 'Maziausiai 4 simboliai').max(7).required(),
+    }),
+    onSubmit: async (values) => {},
+  });
+  console.log('formik.values ===', formik.values);
   return (
-    <form className={css.form}>
+    <form className={css.form} onSubmit={formik.handleSubmit}>
       <h1 className={css.title}>Welcome back,</h1>
 
       <label className={css.label}>
         <span className={css.span}>Email</span>
-        <input className={css.input} type='email' />
+        <input
+          type='email'
+          onChange={formik.handleChange}
+          onBlur={formik.handleBlur}
+          value={formik.values.email}
+          className={formik.touched.email && formik.errors.email ? css.errorInput : css.input}
+          name='email'
+          placeholder='Your email'
+        />
       </label>
+      <p className={css.errorMsg}>{formik.errors.email}</p>
       <label className={css.label}>
         <span className={css.span}>Password</span>
-        <input className={css.input} type='password' />
+        <input
+          type='password'
+          onChange={formik.handleChange}
+          onBlur={formik.handleBlur}
+          value={formik.values.password}
+          className={formik.touched.password && formik.errors.password ? css.errorInput : css.input}
+          name='password'
+          placeholder='Your password'
+        />
       </label>
       {/* <input className={css.input} type='email' />
       <input className={css.input} type='password' /> */}
-      <p class={css.forgot_pass}>Forgot password?</p>
+      <p className={css.errorMsg}>{formik.errors.password}</p>
+      <p className={css.forgot_pass}>Forgot password?</p>
       <button className={css.btn} type='submit'>
         Login
       </button>
