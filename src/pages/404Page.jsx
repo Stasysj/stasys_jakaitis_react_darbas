83ba5c80145a6cedc743fe2594a9060468e748f8
Stasys <srasys@st.lt>
2022-07-06 14:06:43 +0300

    readme_ok

--- src/pages/404Page.jsx
+++ src/pages/404Page.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 function Page() {
   return (
     <>
-      <h2>Jus neprisujungęs...</h2>
+      <h2>Jus neprisjungęs...</h2>
 
       <Link to={'/login'}>Prisijungti čia...</Link>
     </>
