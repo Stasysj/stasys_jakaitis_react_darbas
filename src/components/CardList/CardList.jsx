5f73171bafeb33622f44257b4c779e2ff0a8ca02
Stasys <srasys@st.lt>
2022-07-06 13:07:15 +0300

    search_ok

--- src/components/CardList/CardList.jsx
+++ src/components/CardList/CardList.jsx
@@ -5,22 +5,31 @@ import { useState } from 'react';
 import { useAuthCtx } from '../../store/authContext';
 import { baseUrl, getFetchAuth } from '../../utils';
 import Card from '../Card/Card';
-import { Link } from 'react-router-dom';
-import Search from '../Search/Search';
+
+const filterSkills = (searchText, duomenuMasyvas) => {
+  if (!searchText) {
+    return duomenuMasyvas;
+  }
+  return duomenuMasyvas.filter(({ title }) =>
+    title.toLowerCase().includes(searchText.toLowerCase())
+  );
+};
 
 function CardList() {
   const { token } = useAuthCtx();
   const [skills, setSkills] = useState('');
+  const [filt, setFilt] = useState([]);
   const [error, setError] = useState('');
-  // const [data, setData] = useState(false);
+  const [searchTerm, setSearchTerm] = useState('');
 
-  console.log('token', token);
+  // console.log('token', token);
 
   const getSkills = async () => {
     const fetchResult = await getFetchAuth(`${baseUrl}/content/skills`, token);
     console.log('fetchResult ===', fetchResult);
     if (Array.isArray(fetchResult)) {
       setSkills(fetchResult);
+      setFilt(fetchResult);
       setError('');
     } else {
       setError(fetchResult.error);
@@ -30,18 +39,37 @@ function CardList() {
   useEffect(() => {
     if (token) getSkills();
   }, []);
+
+  useEffect(() => {
+    const filteredSkills = filterSkills(searchTerm, skills);
+    setFilt(filteredSkills);
+  }, [searchTerm]);
+
   return (
     <div className={css.cardList}>
       <h1 className={css.title}>Skills:</h1>
-      <Search />
+      <>
+        <span>
+          <i className={`fa fa-search ${css.icon}`} aria-hidden='true'></i>
+        </span>
+        <input
+          onChange={(e) => setSearchTerm(e.target.value)}
+          value={searchTerm}
+          className={css.search}
+          type='text'
+          placeholder='Find...'
+        />
+      </>
 
       <div className={css.card_container}>
-        {error ? (
+        {!filt ? (
+          <h1>Loading...</h1>
+        ) : error ? (
           <h1>{error}</h1>
-        ) : skills.length === 0 ? (
-          <h1>Lauding...</h1>
+        ) : filt.length === 0 ? (
+          <h1>Skillsu nera.</h1>
         ) : (
-          skills.length > 0 && skills.map((skObj) => <Card key={skObj.id} {...skObj} />)
+          filt.length > 0 && filt.map((skObj) => <Card key={skObj.id} {...skObj} />)
         )}
       </div>
     </div>
